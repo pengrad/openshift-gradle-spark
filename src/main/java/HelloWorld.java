a63f0705f90815a534828ba20d19561c37db4b01
stas <pengrad@bk.ru>
2015-08-03 23:22:36 +0300

    OpenShift ip and port

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,8 +3,9 @@ import static spark.Spark.*;
   public class HelloWorld {
         public static void main(String[] args) {
 
-	    port(8080);	
-//	    port(Integer.parseInt(args[0]));
+	    ipAddress(args[0]);
+//	    port(8080);	
+	    port(Integer.parseInt(args[1]));
 
             get("/hello", (req, res) -> "Hello World");
         }
